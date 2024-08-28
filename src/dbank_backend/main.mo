4e50c7022de169bf4ad363a9d6ec860cf218d5cc
Wh0mM1 <shaikhnihal92@gmail.com>
2024-08-28 20:22:00 +0530

    first commit

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,49 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+//canistor is just like containers in docker
+// this is just like class(canistor)
+
+
+actor DBank{
+  stable var currentValue:Float=100;
+  // currentValue:=100;
+
+  // Debug.print(debug_show(id));
+
+  stable var startTime=Time.now();
+
+  Debug.print(debug_show(startTime));
+
+  public func topUp(amount:Float){
+    currentValue+=amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  public func bottomDown(amount:Float){
+    let tempAmount:Float=currentValue-amount;
+    if(tempAmount>=0)
+    {
+      currentValue-=amount;
+      Debug.print(debug_show(currentValue));
+    }
+    else
+    {
+      Debug.print("Amount is greater than current amount");
+    }
+  };
+
+  public query func checkValue ():async Float{
+    return currentValue;
+  };
+
+
+  public func compound(){
+    let currentTime=Time.now();
+    let timeElapsedNS=currentTime-startTime;
+    let timeElapsedS=timeElapsedNS/1000000000;
+    currentValue:=currentValue*(1.01 ** Float.fromInt(timeElapsedS));
+    startTime:=currentTime;
+  }
+}
--- src/dbank_frontend/src/App.jsx
+++ src/dbank_frontend/src/App.jsx
@@ -1,29 +1,78 @@
-import { useState } from 'react';
-import { dbank_backend } from 'declarations/dbank_backend';
+import { useState } from "react";
+import { dbank_backend } from "../../declarations/dbank_backend";
+
+let currentAmount;
+
+window.addEventListener("load", async function () {
+  update();
+  console.log(currentAmount);
+});
+
+document
+  // .querySelector("form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const button = event.target.querySelector("#submit-btn");
+
+    button.setAttribute("disabled", true);
+
+    const inputAmount = parseFloat(
+      document.getElementById("input-amount").value
+    );
+    if (inputAmount > 0) await dbank_backend.topUp(inputAmount);
+
+    const outputAmount = parseFloat(
+      document.getElementById("withdrawal-amount").value
+    );
+    if (outputAmount > 0) await dbank_backend.bottomDown(outputAmount);
+
+    dbank_backend.compound();
+
+    update();
+    document.getElementById("input-amount").value = "";
+    document.getElementById("withdrawal-amount").value = "";
+
+    button.removeAttribute("disabled");
+    console.log("Submitted");
+  });
+
+async function update() {
+  currentAmount = await dbank_backend.checkValue();
+  document.getElementById("value").innerText = currentAmount.toFixed(2);
+}
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    dbank_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <div className="container">
+        <img src="dbank_logo.png" alt="DBank logo" width="100" />
+        <h1>
+          Current Balance: $<span id="value"></span>
+        </h1>
+        <div className="divider"></div>
+        <form action="#">
+          <h2>Amount to Top Up</h2>
+          <input
+            id="input-amount"
+            type="number"
+            step="0.01"
+            min="0"
+            name="topUp"
+            // value=""
+          />
+          <h2>Amount to Withdraw</h2>
+          <input
+            id="withdrawal-amount"
+            type="number"
+            name="withdraw"
+            step="0.01"
+            min="0"
+            // value=""
+          />
+          <input id="submit-btn" type="submit" value="Finalise Transaction" />
+        </form>
+      </div>
     </main>
   );
 }
